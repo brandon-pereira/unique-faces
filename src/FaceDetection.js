a1051bd4c4453ba495d701a6dbb272e5ce08d3a0
Brandon Pereira <brandonp@criticalmass.com>
2020-07-05 20:09:43 -0600

    work to face recognition

--- src/FaceDetection.js
+++ src/FaceDetection.js
@@ -5,7 +5,9 @@ import fs from "fs";
 import path from "path";
 
 class FaceDetection {
-  //   constructor() {}
+  constructor() {
+    this._detections = [];
+  }
 
   async init() {
     await loadModels(faceapi);
@@ -22,23 +24,73 @@ class FaceDetection {
       .detectAllFaces(canvas)
       .withFaceLandmarks()
       .withFaceDescriptors();
+
+    detections.forEach(fd => {
+      const bestMatch = this.getBestMatch(fd.descriptor);
+      console.log(bestMatch);
+      if (bestMatch._label === "unknown") {
+        this._detections.push(
+          new faceapi.LabeledFaceDescriptors(
+            this._detections.length.toString(),
+            [fd.descriptor]
+          )
+        );
+      } else {
+        // console.log(bestMatch);
+        // console.log(this._detections[bestMatch._label]);
+        this._detections[
+          bestMatch._label
+        ] = new faceapi.LabeledFaceDescriptors(bestMatch._label, [
+          ...this._detections[bestMatch._label]._descriptors,
+          fd.descriptor,
+        ]);
+      }
+    });
+
     await this.debugImage(canvas, detections, imgPath);
     return detections;
   }
 
+  get faceMatcher() {
+    return this._detections.length
+      ? new faceapi.FaceMatcher(this._detections)
+      : null;
+  }
+
+  getBestMatch(descriptor) {
+    let bestMatch;
+    if (this.faceMatcher) {
+      bestMatch = this.faceMatcher.findBestMatch(descriptor);
+    } else {
+      bestMatch = { _label: "unknown" };
+    }
+    return bestMatch;
+  }
+
   async debugImage(image, detections, fileName) {
     return new Promise(resolve => {
-    faceapi.draw.drawDetections(image, detections);
-    faceapi.draw.drawFaceLandmarks(image, detections);
-    const out = fs.createWriteStream(
-      path.resolve(
-        process.cwd(),
-        "temp",
-        fileName.replace(".", "").replace("/", "")
-      )
-    );
-    const stream = image.createPNGStream();
-    stream.pipe(out);
+      faceapi.draw.drawDetections(image, detections);
+      // faceapi.draw.drawFaceLandmarks(image, detections);
+      detections.forEach((result, i) => {
+        // console.log(result);
+        console.log(this._detections);
+        const bestMatch = this.getBestMatch(result.descriptor);
+        const box = detections[i].detection.box;
+        const drawBox = new faceapi.draw.DrawBox(box, {
+          label: bestMatch.toString(),
+        });
+        drawBox.draw(image);
+      });
+
+      const out = fs.createWriteStream(
+        path.resolve(
+          process.cwd(),
+          "temp",
+          fileName.replace(".", "").replace("/", "")
+        )
+      );
+      const stream = image.createPNGStream();
+      stream.pipe(out);
       stream.on("end", resolve);
     });
   }
